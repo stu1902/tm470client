d43b85bf6b3706f18227c8b0a82f255675aff280
stu1902 <stuart1902@gmail.com>
2022-06-04 16:51:55 +0100

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tm470client',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
